a8be2049741699baad72f505fa2e0cb1e48858d7
Vlas <111683706+Kijoin1@users.noreply.github.com>
2024-12-22 16:14:48 +0200

    1.2

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -2,9 +2,9 @@ import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
 
 export function isValidId(req, res, next) {
-  const { id } = req.params;
+  const { contactId } = req.params;
 
-  if (isValidObjectId(id) !== true) {
+  if (isValidObjectId(contactId) !== true) {
     return next(createHttpError(400, 'ID is not valid'));
   }
 
